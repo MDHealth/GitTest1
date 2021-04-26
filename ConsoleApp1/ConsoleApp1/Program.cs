0a3d583376577a6ac74bd056163e67d8aaac2196
MDHealth <71417773+MDHealth@users.noreply.github.com>
2021-04-27 04:54:25 +1000

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             
-            //Coe was added in GitHub
+            //Code was edited in GitHub
         }
     }
 }
